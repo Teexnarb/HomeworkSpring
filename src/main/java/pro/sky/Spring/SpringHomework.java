298a16865ee8f15782a6f1ca10d70b8a1463b9d0
User <danilgaybeshev@mail.ru>
2024-03-13 17:03:30 +0500

    Initial commit

---
+++ src/main/java/pro/sky/Spring/SpringHomework.java
@@ -0,0 +1,13 @@
+package pro.sky.Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHomework {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHomework.class, args);
+	}
+
+}
